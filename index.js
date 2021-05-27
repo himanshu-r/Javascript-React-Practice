a322bb4a118f43a8fffbcb077b7117efb34403b6
himanshu-r <himanshureddy3@gmail.com>
2021-05-27 19:31:03 +0530

    Created a prompt which will take name and age

---
+++ index.js
@@ -0,0 +1,9 @@
+function bio() {
+var name = prompt("What is your name?");
+var age = prompt("What is your age ?");
+var bio = prompt("Tell me something about yourself");
+
+alert("Hi " + name + "you are " + age + " years old.");
+}
+
+bio();
