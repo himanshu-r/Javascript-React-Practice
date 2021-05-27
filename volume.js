ec38aa83bd7e7b5170be7a43af9c450df32d7091
himanshu-r <himanshureddy3@gmail.com>
2021-05-27 19:55:32 +0530

    Addedd area and modify the volume function

--- volume.js
+++ volume.js
@@ -1,8 +1,13 @@
-function volume(height, length, width) {
-    height += 10;
-    return(height * length * width);
+function area(length, width) {
+    return length*width;
 }
 
+function volume(h,l,w) {
+    var n = (h* area(l,w))
+    return n;
+}
+
+console.log(area(2,3))
 console.log(volume(2,3,6))
 
 
