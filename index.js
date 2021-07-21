06105b799056faf1734b6c0348c417b965b6934b
Himanshu Reddy <60777610+himanshu-r@users.noreply.github.com>
2021-07-21 22:04:33 +0530

    Done with react practice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function bio() {
 var name = prompt("What is your name?");
 var age = prompt("What is your age ?");
-
+var bio = prompt("Tell me something about yourself");
 
 alert("Hi " + name + "you are " + age + " years old.");
 }
